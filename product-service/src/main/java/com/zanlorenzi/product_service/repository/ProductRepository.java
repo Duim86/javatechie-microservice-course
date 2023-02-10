65d0f9ba9d7efd330c38784d319287bc91c2dc23
Duim86 <zanlorenzi@gmail.com>
2023-02-10 12:42:47 -0300

    product-service

---
+++ product-service/src/main/java/com/zanlorenzi/product_service/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.zanlorenzi.product_service.repository;
+
+import com.zanlorenzi.product_service.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
